adea838c1e73e1af8c1ed82282b48220c2adfc9c
Volodymyr Holovetskyi <volodymyrgolovetskyi1995@gmail.com>
2022-02-20 19:17:44 +0100

    implementation method calculation average

--- src/main/java/my/expense/calcuator/payer/application/calculation/strategy/CalculateTheAverageForAllPeople.java
+++ src/main/java/my/expense/calcuator/payer/application/calculation/strategy/CalculateTheAverageForAllPeople.java
@@ -1,16 +1,29 @@
 package my.expense.calcuator.payer.application.calculation.strategy;
 
-import my.expense.calcuator.payer.application.calculation.CalculateExpenseStrategy;
-import my.expense.calcuator.payer.domain.Payer;
+import my.expense.calcuator.payment.domain.Payment;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-public class CalculateTheTotalCostForAllPeople implements CalculateExpenseStrategy {
+public class CalculateTheAverageForAllPeople implements CalculateExpenseStrategy {
 
     @Override
-    public void calculate(BigDecimal averages, List<Payer> payers) {
+    public void calculate(List<SettlementPayer> settlementPayers) {
 
+        for (SettlementPayer payer : settlementPayers) {
+            BigDecimal reduce = payer.getPayments().stream()
+                    .map(Payment::getPayment)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
 
+            if (payer.getPayments().size() > 1){
+                payer.setAverage(getAverage(reduce, payer));
+            }else if (payer.getPayments().size() == 1){
+                payer.setAverage(payer.getPayments().get(0).getPayment());
+            }else payer.setAverage(BigDecimal.ZERO);
+        }
+    }
+
+    private BigDecimal getAverage(BigDecimal reduce, SettlementPayer payer) {
+        return reduce.divide(BigDecimal.valueOf(payer.getPayments().size()));
     }
 }
